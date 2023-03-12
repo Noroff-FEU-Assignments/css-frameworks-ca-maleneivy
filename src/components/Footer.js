4a6e05a26db1315980db8eebef84b1d7ebd2de19
maleneivy <68667843+maleneivy@users.noreply.github.com>
2023-03-12 15:11:42 +0100

    added sass and sticky footer
    sticky footer with bootstrap class

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,10 @@
-import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 function Footer() {
     return (
         <>
-            <Container>
+            <footer class="fixed-bottom">
                 <Row>
                     <Col>
                         Vimeo
@@ -14,7 +13,7 @@ function Footer() {
                     <Col>hello@yay.no</Col>
                     <Col>Copyright 2023</Col>
                 </Row>
-            </Container>
+            </footer>
         </>
     );
 
